4d1d4e36e247739df012bda820813846bc17ce55
Scott Brenner <sebrenner@Scotts-MacBook.local>
2012-03-21 18:56:27 -0400

    Working Class.  Added some getter functions.

--- attorneyDocketClass.php
+++ attorneyDocketClass.php
@@ -13,13 +13,13 @@ class AttorneySchedule
 //     future and past year -- date_range=prior12
 
     // property declaration    
-    const clerkSchedPrintURI = "http://www.courtclerk.org/attorney_schedule_list_print.asp?court_party_id=18161";
+    const clerkSchedPrintURI = "http://www.courtclerk.org/attorney_schedule_list_print.asp?court_party_id=";
     const clerkSchedPrintURIsuffix = "&date_range=prior6";
     
     // An array that holds all the clerkIDs asociated with this attorney.
 //    protected $clerkIDs[ principalClerkID ] = $principalClerkID;
-
-	var $NACs = array();	// An array of all future NAC and the last six months of NAC.
+	protected $html = null;
+	protected $NACs = array();	// An array of all future NAC and the last six months of NAC.
 
 	protected $fName = null;
 	protected $lName = null;
@@ -28,28 +28,23 @@ class AttorneySchedule
 	protected $lastUpdated = null; 
 		
 	// method declarations
-	
-
 	function getAttorneyName( &$schedTable ){
 		$attorneyTable = $schedTable->find('table', 0);
 		$attorney = $attorneyTable->find('td', 1)->innertext;
-		
-		echo "<p>" . $attorney . "</p>";
 		$attorney = explode( "/" , $attorney );
 		$this->lName = $attorney[0];
 		$this->fName = $attorney[1];
 		$this->mName = $attorney[2];
 	}
-
 	function getSchedTble( $uri ){
 		$html = file_get_html( $uri );
 		$schedTable = $html->find('table', 2);
 		return $schedTable;
 	}
- 
  	function getNACs( &$schedTable ){
 		// drop the outermost table
 		$tableOfNACTables = $schedTable->find( 'table', 1);
+
 		// Get the inntertext of the table containing the nac tables
 		// and convert it to an dom object
 		$tableOfNACTables = $tableOfNACTables->innertext;
@@ -60,9 +55,7 @@ class AttorneySchedule
 			$NAC = self::convertNACtoArray( $NACTable );
 			array_push($this->NACs, $NAC );
 		}
-		print_r( $NACs );
 	}
-
 	function convertNACtoArray( &$NACTable ){
 		$NAC= array();
 	
@@ -89,23 +82,32 @@ class AttorneySchedule
 		$location = 	$NACTable->find('td', 5 )->innertext;
 		$NAC[location] = 	substr($location, 27);
 		
-		$desc = 	$NACTable->find('td', 6 )->innertext;
-		$NAC[ desc ]  =	substr( $desc, 30 );
+		$summary = 	$NACTable->find('td', 6 )->innertext;
+		$NAC[ summary ]  =	substr( $summary, 30 );
 		
 		return $NAC;
 	}
- 
 	function __construct( $principalClerkID ) {
 		// Get the date (html) from the URI (Clerk's site)
-		$uri = self::clerkSchedPrintURI . $clerkID . self::clerkSchedPrintURIsuffix;
-		echo $uri;
+		$uri = self::clerkSchedPrintURI . $principalClerkID . self::clerkSchedPrintURIsuffix;
 		$schedTable = self::getSchedTble( $uri );
 		
 		self::getAttorneyName( $schedTable );
 		self::getNACs( $schedTable );
 	}
+	function getAttorneyLName(){
+		return $this->lName;
+	}
+	function getAttorneyFName(){
+		return $this->fName;
+	}
+	function getAttorneyMName(){
+		return $this->mName;
+	}
+	
 }
 
-$attorneySchedule =  new AttorneySchedule( "18161" );
+$attorneySchedule =  new AttorneySchedule( "73125" );
+echo "<h1>" . $attorneySchedule->getAttorneyFName() . $attorneySchedule->getAttorneyMName() .$attorneySchedule->getAttorneyLName() . "</h1>";
 print_r( $attorneySchedule );
 ?>
