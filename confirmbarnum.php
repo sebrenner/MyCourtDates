87278a86dacad4b2ffd3f97702fafc52b9f91eef
Scott Brenner <scott@scottbrenner.com>
2012-05-10 22:50:52 -0400

    Started to implement site usign the themeforest template.

---
+++ confirmbarnum.php
@@ -0,0 +1,339 @@
+<!doctype html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Enter BarNumber</title>
+	
+	<meta name="description" content="">
+	<meta name="author" content="revaxarts.com">
+	
+	
+	<!-- Google Font and style definitions -->
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans:regular,bold">
+	<link rel="stylesheet" href="css/style.css">
+	
+	<!-- include the skins (change to dark if you like) -->
+	<link rel="stylesheet" href="css/light/theme.css" id="themestyle">
+	<!-- <link rel="stylesheet" href="css/dark/theme.css" id="themestyle"> -->
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<link rel="stylesheet" href="css/ie.css">
+	<![endif]-->
+	
+	<!-- Apple iOS and Android stuff -->
+	<meta name="apple-mobile-web-app-capable" content="no">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+	
+	<!-- Apple iOS and Android stuff - don't remove! -->
+	<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,maximum-scale=1">
+	
+	<!-- Use Google CDN for jQuery and jQuery UI -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js"></script>
+	
+	<!-- Loading JS Files this way is not recommended! Merge them but keep their order -->
+	
+	<!-- some basic functions -->
+	<script src="js/functions.js"></script>
+		
+	<!-- all Third Party Plugins and Whitelabel Plugins -->
+	<script src="js/plugins.js"></script>
+	<script src="js/editor.js"></script>
+	<script src="js/calendar.js"></script>
+	<script src="js/flot.js"></script>
+	<script src="js/elfinder.js"></script>
+	<script src="js/datatables.js"></script>
+	<script src="js/wl_Alert.js"></script>
+	<script src="js/wl_Autocomplete.js"></script>
+	<script src="js/wl_Breadcrumb.js"></script>
+	<script src="js/wl_Calendar.js"></script>
+	<script src="js/wl_Chart.js"></script>
+	<script src="js/wl_Color.js"></script>
+	<script src="js/wl_Date.js"></script>
+	<script src="js/wl_Editor.js"></script>
+	<script src="js/wl_File.js"></script>
+	<script src="js/wl_Dialog.js"></script>
+	<script src="js/wl_Fileexplorer.js"></script>
+	<script src="js/wl_Form.js"></script>
+	<script src="js/wl_Gallery.js"></script>
+	<script src="js/wl_Multiselect.js"></script>
+	<script src="js/wl_Number.js"></script>
+	<script src="js/wl_Password.js"></script>
+	<script src="js/wl_Slider.js"></script>
+	<script src="js/wl_Store.js"></script>
+	<script src="js/wl_Time.js"></script>
+	<script src="js/wl_Valid.js"></script>
+	<script src="js/wl_Widget.js"></script>
+	
+	<!-- configuration to overwrite settings -->
+	<script src="js/config.js"></script>
+		
+	<!-- the script which handles all the access to plugins etc... -->
+	<script src="js/script.js"></script>
+	
+	
+</head>
+<body>
+				<div id="pageoptions">
+			<ul>
+				<li><a href="login.html">Logout</a></li>
+				<li><a href="#">Settings</a></li>
+			</ul>
+			<div>
+						<h3>Place for some configs</h3>
+						<p>Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores.</p>
+			</div>
+		</div>
+
+			<header>
+		<div id="logo">
+			<a href="dashboard.html">Logo Here</a>
+		</div>
+		<div id="header">
+			<ul id="headernav">
+				<li><ul>
+					<li><a href="icons.html">Icons</a><span>300+</span></li>
+					<li><a href="#">Submenu</a><span>4</span>
+						<ul>
+							<li><a href="#">Just</a></li>
+							<li><a href="#">another</a></li>
+							<li><a href="#">Dropdown</a></li>
+							<li><a href="#">Menu</a></li>
+						</ul>
+					</li>
+					<li><a href="login.html">Login</a></li>
+					<li><a href="wizard.html">Wizard</a><span>Bonus</span></li>
+					<li><a href="#">Errorpage</a><span>new</span>
+						<ul>
+							<li><a href="error-403.html">403</a></li>
+							<li><a href="error-404.html">404</a></li>
+							<li><a href="error-405.html">405</a></li>
+							<li><a href="error-500.html">500</a></li>
+							<li><a href="error-503.html">503</a></li>
+						</ul>
+					</li>
+				</ul></li>
+			</ul>
+			<div id="searchbox">
+				<form id="searchform" autocomplete="off">
+					<input type="search" name="query" id="search" placeholder="Search">
+				</form>
+			</div>
+			<ul id="searchboxresult">
+			</ul>
+		</div>
+	</header>
+
+				<nav>
+			<ul id="nav">
+				<li class="i_house"><a href="dashboard.html"><span>Dashboard</span></a></li>
+				<li class="i_book"><a><span>Documentation</span></a>
+					<ul>
+						<li><a href="doc-alert.html"><span>Alert Boxes</span></a></li>
+						<li><a href="doc-breadcrumb.html"><span>Breadcrumb</span></a></li>
+						<li><a href="doc-calendar.html"><span>Calendar</span></a></li>
+						<li><a href="doc-charts.html"><span>Charts</span></a></li>
+						<li><a href="doc-dialog.html"><span>Dialog</span></a></li>
+						<li><a href="doc-editor.html"><span>Editor</span></a></li>
+						<li><a href="doc-file.html"><span>File</span></a></li>
+						<li><a href="doc-fileexplorer.html"><span>Fileexplorer</span></a></li>
+						<li><a href="doc-form.html"><span>Form</span></a></li>
+						<li><a href="doc-gallery.html"><span>Gallery</span></a></li>
+						<li><a href="doc-inputfields.html"><span>Inputfields</span></a></li>
+						<li><a href="doc-slider.html"><span>Slider</span></a></li>
+						<li><a href="doc-store.html"><span>Store</span></a></li>
+						<li><a href="doc-widget.html"><span>Widget</span></a></li>
+					</ul>
+				</li>
+				<li class="i_create_write"><a href="form.html"><span>Form</span></a></li>
+				<li class="i_graph"><a href="charts.html"><span>Charts</span></a></li>
+				<li class="i_images"><a href="gallery.html"><span>Gallery</span></a></li>
+				<li class="i_blocks_images"><a href="widgets.html"><span>Widgets</span></a></li>
+				<li class="i_breadcrumb"><a href="breadcrumb.html"><span>Breadcrumb</span></a></li>
+				<li class="i_file_cabinet"><a href="fileexplorer.html"><span>Fileexplorer</span></a></li>
+				<li class="i_calendar_day"><a href="calendar.html"><span>Calendar</span></a></li>
+				<li class="i_speech_bubbles_2"><a href="dialogs_and_buttons.html"><span>Dialogs &amp; Buttons</span></a></li>
+				<li class="i_table"><a href="datatable.html"><span>Table</span></a></li>
+				<li class="i_typo"><a href="typo.html"><span>Typo</span></a></li>
+				<li class="i_grid"><a href="grid.html"><span>Grid</span></a></li>
+			</ul>
+		</nav>
+		
+			
+		<section id="content">
+        <?php 
+        // 
+        //  confirmSubscriberId.php
+        //  MyCourtDates
+        //
+        //  This php script queries the clerk's site 
+        //   (courtclerk.org/attorney_schedule_list_print.asp?
+        //    court_party_id=69587&date_range=) 
+        //  using the Bar ID, returns the attorney name and the next three event.  
+        //  Then it displays the events and asks the user to confirm that the bar 
+        //  number is correct.
+        //  
+        //  Created by Scott Brenner on 2012-04-08.
+        //  Copyright 2012 Scott Brenner. All rights reserved.
+        // 
+
+        require_once("class.user.php");
+        require_once("functions.case.php");
+        // This code declares the time zone
+        ini_set('date.timezone', 'America/New_York');
+        date_default_timezone_set ('America/New_York');
+
+
+        // Get parameters
+        if( isset( $_GET['barNumber'] ) ){
+            $attorneyId = strtoupper( $_GET['barNumber'] );
+        }
+        else{
+            echo "\t\tusing default value for testing.\n";
+            $attorneyId = '73125';
+        }
+
+        if( isset( $_GET['sumStyle'] ) ){
+            $sumStyle = $_GET['sumStyle'];
+        }
+        else{
+          echo "error no sumStyle was provided.  Using default style.";
+          $sumStyle = 'csn';
+        }
+
+        if( isset( $_GET['reminders'] ) ){
+            $reminders = $_GET['reminders'];
+            if( isset( $_GET['reminderInterval'] ) ){
+                $reminderInterval = $_GET['reminderInterval'];
+            }
+            else{
+              echo "error no reminderInterval was provided.  Using default - 15 minutes.";
+              $reminderInterval = '15 minutes';
+            }
+        }
+        else{
+          echo "No reminders requested.";
+        }
+
+        // The user object
+        // function __construct($userBarNumber, $verbose, $sourceFlag = self::LOGIC, $rangeFlag = self::FUTURE)
+
+        $a =  new user($attorneyId, false);
+
+        // Output attorney name and next three events.
+        echo "<h2>The Clerk of Court's website indicates that attorney id: $attorneyId belongs to ". 
+                $a->getFName() . ' ' . $a->getMName() . ' ' . $a->getLName() . '.</h2>';
+
+        echo "<h4>The Hamilton County Clerk of Court's lists " . $a->getActiveNACCount() . " active scheduled court appearances.</h4>";
+
+        // Query db, if barnumber is already in the database, then show the old prefs and the new prefs.
+        echo "this space will show old prefs if prefs are being updated.";
+
+        echo "<h4>The next <strong>three</strong> scheduled court appearances:</h4>";
+
+        // Print a table list of active NACs
+        echo "<table border=\"1\">
+          <th>Date</th>
+          <th>CaseNumber</th>
+          <th>Caption</th>
+          <th>Setting</th>
+          <th>Location</th>
+          <th>AttorneyID</th>
+          </tr>";
+
+        $counter = 0;
+        foreach ($a->getUserSchedule() as $NAC ) {
+            // skip past NACs
+            if ( date( "Y m d", strtotime($NAC['timeDate'])) < date( "Y m d" ) ) {continue;}
+
+            if ( $NAC["active"]) {
+                echo "<tr>";
+            }else{
+                echo "<TR BGCOLOR=\"#99CCFF\">";
+            };
+            echo "
+              <td>" . date( "F j, Y, g:i a",  strtotime($NAC['timeDate']) ) . "</td>
+              <td><a href=\"" . getHistURI( $NAC['caseNumber']) . "\">" .  $NAC['caseNumber'] . "</a></td>
+              <td>" . $NAC['plaintiffs'] . " v. " . $NAC['defendants'] ."</td>
+              <td>" . $NAC['setting'] ."</td>
+              <td>" . $NAC['location'] ."</td>
+              <td>" . "<a href=\"" .
+                      "http://www.courtclerk.org/attorney_schedule_list_print.asp?court_party_id=" . 
+                      $attorneyId . 
+                      "&date_range=prior6\">" .
+                      $attorneyId .
+                      "</a></td></tr>";
+            $counter ++;
+            // only show the next 3 NACs 
+            if ( $counter > 2 ) {  break; }
+        }
+        echo "</table>";
+
+        // Output confirmation form inputs
+        ?>
+        <h2>Links:</h2>
+        <?php
+        echo "<p>Add schedule to <a href='webcal://ics.php?id=$attorneyId'>iPhone</a></p>";
+        $gCalURL='http://www.google.com/calendar/render?cid=' . rawurlencode("http://mycourtdates.com/ics.php?id=$attorneyId");
+        echo "<p>Add schedule to <a href='$gCalURL' target='_blank'>Google Calendar</a></p>";
+        echo "<p>Add schedule to <a href='ics.php?id=$attorneyId'>Outlook</a></p>";
+        echo "<p><a href='ics.php?id=$attorneyId'>iCal Feed</a></p>";
+        echo "<p><a href='json.php?id=$attorneyId'>JSON Feed</a></p>";
+        ?>
+        <h2>Wrong Schedule?</h2>
+            <p>If this attorney schedule is not the correct one you can search again:</p>
+            <form action="confirmbarnumber.php" method="get" accept-charset="utf-8">
+                <label for="barNumber">Bar Number</label><input type="text" name="barNumber" id="barNumber">
+                <h2>Summary Style</h2>
+                <h5>This is text that will appear as the title of the Court settings.</h5>
+                <fieldset>
+                  <legend>Preferences</legend>
+                <table class="form-table">
+                	<tr>
+                		<td align='left'><label><input name="sumStyle" type="radio" checked value="csn" class="sumStyle"  /> Default (<emp>Caption ~ Full  Setting ~ Case Number</emp>)</label></td>
+                		<td>e.g., <code>Marbury v. Madison ~ Motion For Summary Judgment ~ A 1308713</code></td>
+                	</tr>
+                	<tr>
+                		<td align='left'><label><input name="sumStyle" type="radio" value="da" class="sumStyle"  /> Prosecutor/Criminal Defense Style (<emp>Defendant ~ Abbreviated Setting</emp>)</label></td>
+                		<td>e.g., <code>Madison ~ DSC</code></td>
+                	</tr>
+                	<tr>
+                		<td align='left'><label><input name="sumStyle" type="radio" value="can" class="sumStyle"  /> Abbreviated (<emp>Caption ~ Abbreviated Setting ~ Case Number</emp>)</label></td>
+                		<td>e.g., <code>Marbury v. Madison ~ FE ~ A 1308713</code></td>
+                	</tr>
+                	<tr>
+                		<td align='left'><label><input name="sumStyle" type="radio" value="cal" class="sumStyle"  /> Abbreviated (<emp>Caption ~ Abbreviated Setting ~ Location</emp>)</label></td>
+                		<td>e.g., <code>Marbury v. Madison ~ FE ~ H.C. Court House Room 485</code></td>
+                	</tr>
+
+                  <!-- <tr>
+                      <td align='left'>
+                          <label><input name="sumStyle" id="custom_sumStyle" type="radio" value="custom" class="sumStyle"  checked='checked' />
+                          Custom Structure</label>
+                      </td>
+                      <td>
+                                      <input name="permalink_structure" id="permalink_structure" type="text" value="/%category%/%postname%" class="regular-text code" />
+                      </td>
+                  </tr> -->
+                </table>
+            </fieldset>          
+                <h2>Reminders</h2>
+                <h4>Indicate whether you want your calendar to remind you of scheduled events:</h4>
+                <tr>
+                    <td align='left'>
+                  <label><input name="reminders" type="checkbox" value="true" class="reminders"  /> Reminders (<emp>Caption ~ Abbreviated Setting ~ Location</emp>)</label>
+                    </td>
+                    <td>
+                        <input name="reminderInterval" id="reminderInterval" type="text" value="15 minutes" class="regular-text code" />
+                    </td>
+                </tr>
+
+                <p><input type="submit" value="Continue &rarr;"></p>
+            </form>
+<!-- end div #content -->
+		<footer>Copyright by revaxarts.com 2012</footer>
+</body>
+</html>
